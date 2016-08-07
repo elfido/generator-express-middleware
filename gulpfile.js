df399b704124f8a3744cba9d5321bc37f83bed1d
Fidencio Garrido <fido07@yahoo.com.mx>
2016-08-06 17:05:11 -0700

    Adding delta files before release

--- gulpfile.js
+++ gulpfile.js
@@ -59,8 +59,8 @@ var generatorHelpers = {
 	getBumpOption: function(){
 		let args = process.argv,
 			res = {type: "patch"},
-			opt = (args.length>3) ? args[3] : null;
-		console.log(args.length,opt, args);
+			opt = (args.length>2) ? args[3] : null;
+		console.log(opt, args);
 		if ( opt !== null && generatorHelpers.bumpParams.indexOf(opt)>=0 ){
 			opt = opt.replace("--", "");
 			res = {type: opt};
