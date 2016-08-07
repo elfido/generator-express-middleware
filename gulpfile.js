99093cd55cbe5dde66d17442de215f0e4770745d
Fidencio <fidencio.garrido@gmail.com>
2016-08-07 07:55:35 -0700

    Cleanning for NodeJS 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,10 @@
 "use strict";
-var gulp = require("gulp"),
-	bump = require("gulp-bump"),
-	exec = require("child_process").exec,
-	fs = require("fs"),
+var bump = require("gulp-bump"), 
+	gulp = require("gulp"),
+	helpers = require("./utils/helpers"),
 	jshint = require("gulp-jshint"),
-	watch = require("gulp-watch"),
-	mocha = require("gulp-mocha");
-
-var generatorHelpers = {
-	bumpParams: ["--patch", "--minor", "--major", "--prerelease"],
-	tag (){
-		let info = JSON.parse(fs.readFileSync("package.json")),
-			add = `git add . && git commit -m "Adding delta files before release"`,
-			commit = `git tag -a v${info.version} -m "Tag ${info.version}"`,
-			push = `git push origin v${info.version}`;
-		generatorHelpers.run(add, function(){
-			console.log("Adding all pending files");
-			generatorHelpers.run(commit, function(){
-				console.log("Commit complete");
-				generatorHelpers.run(push, function(){
-					console.log("Tag pushed to origin");	
-				});
-			});
-		});
-	},
-	run (cmd, cb){
-		exec(cmd, function(err, stdout, stderr){
-			if(!err){
-				cb();
-			} else{
-				console.dir(err);
-			}
-		});
-	},
-	getBumpOptionn (){
-		let args = process.argv,
-			res = {type: "patch"},
-			opt = (args.length>2) ? args[3] : null;
-		if ( opt && opt !== null && generatorHelpers.bumpParams.indexOf(opt)>=0 ){
-			opt = opt.toLowerCase().replace("--", "");
-			res = {type: opt};
-		}
-		if ( opt && opt !== null && opt.indexOf("--version") === 0){
-			let _version = opt.split(":");
-			if (_version.length<1){
-				console.error("Invalid version");
-			}
-			res = {version: _version[1]};
-		}
-		return res;
-	}
-};
+	mocha = require("gulp-mocha"),
+	watch = require("gulp-watch");
 
 gulp.task("livetest", function(){
 	return watch("test/**/*.spec.js", {})
@@ -77,12 +31,12 @@ gulp.task("jshint", function() {
 });
 
 gulp.task("bump", function(){
-	let type = generatorHelpers.getBumpOption();
+	let type = helpers.getBumpOption();
 	gulp.src("./package.json")
 	.pipe(bump( type ))
 	.pipe(gulp.dest("./"))
 	.on("end", function(){
-		generatorHelpers.tag();
+		helpers.tag();
 	});
 });
 
