e1c63afa38513e3ebedff4ad11e8d98b94da653c
Fidencio Garrido <fido07@yahoo.com.mx>
2016-08-06 17:28:27 -0700

    Adding delta files before release

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,10 +1,16 @@
 "use strict";
 var gulp = require("gulp"),
-	jshint = require("gulp-jshint"),
 	bump = require("gulp-bump"),
-	exec = require("gulp-exec"),
 	fs = require("fs"),
+	jshint = require("gulp-jshint"),
+	exec = require("gulp-exec"),
+	watch = require("gulp-watch"),
 	mocha = require('gulp-mocha');
+
+gulp.task("livetest", function(){
+	return watch(["test/**/*.spec.js", "index.js"], {})
+	.pipe(gulp.dest("test"));
+});
 
 gulp.task("test", function(cb){
 	let mochaErr ;
@@ -29,24 +35,47 @@ gulp.task("bump", function(){
 	let type = generatorHelpers.getBumpOption();
 	gulp.src("./package.json")
 	.pipe(bump( type ))
-	.pipe(gulp.dest('./'));
-});
-
-gulp.task("gittag", function(){
-	let info = fs.readFileSync("package.json");
+	.pipe(gulp.dest('./'))
+	.on("end", function(){
+		generatorHelpers.tag();
+	});
 });
 
 var generatorHelpers = {
 	bumpParams: ["--patch", "--minor", "--major", "--prerelease"],
+	tag: function(){
+		let info = JSON.parse(fs.readFileSync("package.json")),
+			add = `git add . && git commit -m "Adding delta files before release"`,
+			commit = `git tag -a v${info.version} -m "Tag ${info.version}"`,
+			push = `git push origin v${info.version}`;
+		generatorHelpers.run(add, function(){
+			console.log("Adding all pending files");
+			generatorHelpers.run(commit, function(){
+				console.log("Commit complete");
+				generatorHelpers.run(push, function(){
+					console.log("Tag pushed to origin");	
+				});
+			});
+		});
+	},
+	run: function(cmd, cb){
+		exec(cmd, function(err, stdout, stderr){
+			if(!err){
+				cb();
+			} else{
+				console.dir(err);
+			}
+		});
+	},
 	getBumpOption: function(){
 		let args = process.argv,
 			res = {type: "patch"},
-			opt = (args.length>2) ? args[3].toLocaleLowerCase() : null;
-		if ( opt !== null && generatorHelpers.bumpParams.indexOf(opt)>=0 ){
-			opt = opt.replace("--", "");
+			opt = (args.length>2) ? args[3] : null;
+		if ( opt && opt !== null && generatorHelpers.bumpParams.indexOf(opt)>=0 ){
+			opt = opt.toLowerCase().replace("--", "");
 			res = {type: opt};
 		}
-		if ( opt !== null && opt.indexOf("--version") === 0){
+		if ( opt && opt !== null && opt.indexOf("--version") === 0){
 			let _version = opt.split(":");
 			if (_version.length<1){
 				console.error("Invalid version");
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 "use strict";
 var gulp = require("gulp"),
-	util = require("gulp-util"),
-	fs = require("fs"),
-	exec = require('child_process').exec,
 	bump = require("gulp-bump"),
+	exec = require('child_process').exec,
+	fs = require("fs"),
+	jshint = require("gulp-jshint"),
+	watch = require("gulp-watch"),
 	mocha = require('gulp-mocha');
+
+gulp.task("livetest", function(){
+	return watch("test/**/*.spec.js", {})
+	.pipe(gulp.dest("test"));
+});
 
 gulp.task("test", function(cb){
 	let mochaErr ;
@@ -17,6 +23,12 @@ gulp.task("test", function(cb){
 	.on("end", function () {
 		cb(mochaErr);
 	});
+});
+
+gulp.task("jshint", function() {
+  return gulp.src(["index.js", "test/**/*.js", "src/**/*.js"])
+	.pipe(jshint())
+	.pipe(jshint.reporter("default"));
 });
 
 gulp.task("bump", function(){
